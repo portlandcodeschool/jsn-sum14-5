0429519271d95b801f57e818fe9b0ee6a699e44a
Geoffrey Unger <geoffunger@gmail.com>
2014-06-14 19:12:57 -0700

    most of problem 2 in place and working

---
+++ GeoffUnger/problem-2.js
@@ -0,0 +1,88 @@
+/**
+ * Created with PhpStorm.
+ * User: geoffreyunger
+ * Date: 6/14/14
+ * Time: 4:07 PM
+ */
+
+function TicTacToeBoard() {
+    var cells = [];
+    var xPos = 0;
+    var yPos = 0;
+    var gridScore = [0,0,0,0,0,0,0,0];
+    for (i = 0; i <= 8; i++) {
+        cells[i] = {};
+        cells[i].xPos = xPos;
+        cells[i].yPos = yPos;
+        cells[i].contents = "e";
+        xPos++;
+        if (xPos > 2) {
+            yPos++;
+            xPos = 0;
+        }
+    }
+    this.show = function () {
+        var count = 0;
+        var row = "";
+        for (i = 0; i < 9; i++) {
+            row += (cells[i].contents + " ");
+            count++;
+            if (count == 3) {
+                row += "\n";
+                count = 0;
+            }
+        }
+        console.log(row);
+    }
+    this.placeX = function (x, y) {
+        var success = true;
+        cells.map(function (item) {
+
+            if ((item.xPos == x) && (item.yPos == y)) {
+                if (item.contents != "e") success = undefined;
+                else {
+                    item.contents = "x";
+                    gridScore[x] += 1;
+                    gridScore[3 + y] += 1;
+                    if (x == y) gridScore[7] += 1;
+                    if ((2 - x) == y) gridScore[8] += 1;
+                }
+            }
+
+        })
+        return success;
+    }
+    this.placeO = function (x, y) {
+        cells.map(function (item) {
+            if ((item.xPos == x) && (item.yPos == y)) {
+                if (item.contents != "e") return undefined;
+                item.contents = "o";
+                gridScore[x] -= 1;
+                gridScore[3 + y] -= 1;
+                if (x == y) gridScore[6] -= 1;
+                if ((2 - x) == y) gridScore[7] -= 1;
+
+            }
+
+        })
+        return true;
+    }
+    this.clear = function () {
+        cells.map(function (item) {
+            item.contents = "e";
+        })
+        gridScore = [0,0,0,0,0,0,0,0];
+    }
+    this.winner = function () {
+        gridScore.map(function (item) {
+            console.log(item);
+            if((item == -3) || (item == 3)){console.log("We have a winner!")};
+
+        })
+
+    }
+
+
+
+}
+
