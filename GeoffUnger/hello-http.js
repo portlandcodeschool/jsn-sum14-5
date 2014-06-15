e81c7c3116bc504fd977da51d8f3c27d46caa934
Geoffrey Unger <geoffunger@gmail.com>
2014-06-15 14:44:40 -0700

    Minor refinements to Node http server name

--- GeoffUnger/hello-http.js
+++ GeoffUnger/hello-http.js
@@ -5,14 +5,14 @@
 var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
+var myServer = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end("<h1>Hello World\n</h1>");
     console.log("Someone just pinged me!");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+myServer.listen(8000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
