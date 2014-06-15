ec2e3385bd853fe28530a6cbb07006066218ffa1
Geoffrey Unger <geoffunger@gmail.com>
2014-06-15 13:59:21 -0700

    Made example slightly fancier, with HTML and server-side 'updates'

--- GeoffUnger/hello-http.js
+++ GeoffUnger/hello-http.js
@@ -6,8 +6,9 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("<h1>Hello World\n</h1>");
+    console.log("Someone just pinged me!");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
