12b0eb7a9094aace10e948b938eedd3828618ad3
hannaliebl <hanna@hannaliebl.com>
2014-06-15 23:59:32 -0700

    more correct answer to problem 3 parts a and b

--- hannaliebl/homework-5-problem-3.js
+++ hannaliebl/homework-5-problem-3.js
@@ -1,11 +1,14 @@
-var makeCard = (function () {
-    function Card (id) {
+//PART A:
+
+var Card = (function () {
+    function makecard (id) {
+        var card = {};
         this.id = id;
         this.isvalid = Card.isvalid;
         this.rank = Card.rank;
         this.suit = Card.suit;
     }
-    Card.isvalid = function(num, low, high) {
+    makecard.isvalid = function(num, low, high) {
         if ((typeof num) != "number")
             return NaN;
         if (!Number.isInteger(num))
@@ -14,16 +17,87 @@ var makeCard = (function () {
             return NaN;
         return true;
     }
-    Card.rank = function () {
+    makecard.rank = function () {
         return this.isvalid(this.id, 0, 51) && Math.floor(this.id / 4) + 1;
     }
-    Card.suit = function () {
+    makecard.suit = function () {
        return this.isvalid(this.id, 0, 51) && (this.id % 4) + 1;
     }
-    return Card;
+    return makecard;
 })();
 
-var card1 = new makeCard(1);
+var card1 = new Card(1);
 card1.rank();
 card1.suit();
 card1.constructor; //test
+
+////////////////////////////////////////////////////////////////
+
+//PART B 
+//(one way):
+
+var Card = (function () {
+    function makecard (id) {
+        var card = {};
+        this.id = id;
+    }
+    return makecard;
+})();
+
+Card.prototype = {
+isvalid: function(num, low, high) {
+    if ((typeof num) != "number")
+        return NaN;
+    if (!Number.isInteger(num))
+        return NaN;
+    if (num < low || num > high)
+        return NaN;
+    return true;
+   },
+rank: function () {
+      return this.isvalid(this.id, 0, 51) && Math.floor(this.id / 4) + 1;
+   },
+suit: function () {
+      return this.isvalid(this.id, 0, 51) && (this.id % 4) + 1;
+   }
+}
+
+var card2 = new Card(2);
+card2.rank();
+card2.suit();
+card2 instanceof Card //true
+
+////////////////////////////////////////
+
+//PART B 
+//(another way, perhaps less clear):
+
+var Card = (function () {
+    function makecard (id) {
+        var card = {};
+        this.id = id;
+    }
+    makecard.prototype = {
+    isvalid: function(num, low, high) {
+        if ((typeof num) != "number")
+            return NaN;
+        if (!Number.isInteger(num))
+            return NaN;
+        if (num < low || num > high)
+            return NaN;
+        return true;
+       },
+    rank: function () {
+          return this.isvalid(this.id, 0, 51) && Math.floor(this.id / 4) + 1;
+       },
+    suit: function () {
+          return this.isvalid(this.id, 0, 51) && (this.id % 4) + 1;
+       }
+    }
+    return makecard;
+})();
+
+var card2 = new Card(2);
+card2.rank();
+card2.suit();
+card2 instanceof Card //true
