6cd3c773e113826dc1c7df0557be967a0e14ecb3
Action_Center <ac@Action-Centers-Mac-Pro.local>
2014-06-17 15:52:28 -0700

    almost done with 5.2

--- jmej/hw5.js
+++ jmej/hw5.js
@@ -60,51 +60,56 @@ existing properties of it's prototype object - so those proto/prototype links re
 var ticTacToe = (function(){
 var squares = ["*", "*", "*", "*", "*", "*", "*", "*", "*"];
 var address = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
-
-function TicTacToeBoard(){
-	this.move = function(x, y, player){
+ 
+function TicTacToeBoard(callbackFn){
+    this.move = function(x, y, player){
         var newMove = String(x)+String(y);
-		var square = address.indexOf(newMove);
-		if (squares[square] != '*'){
+        var square = address.indexOf(newMove);
+        if (squares[square] != '*'){
             this.show();
-		    console.log(squares[square]+" already made that move!");
+            console.log(squares[square]+" already made that move!");
             }
-		else{
+        else{
             squares[square] = player; 
             console.log("nice move "+player);
             this.show();
-		    this.win();
+            this.win();
             }
-	}
-	this.win = function(){
-        for (var i = 0; i<9; i++){
-        if (i % 3 == 0)   
-           if(squares[i] == squares[i+1])
-              if (squares[i+2] == squares [i])
-                if (squares[i] != "*")
-          {console.log(squares[1] + " is the winner!");}
+    }
+    this.win = function(){
+        for (var i = 0; i<3; i++){       
+        if ((((squares[4] == squares[8]) && (squares[4] == squares [0])) || ((squares[4] == squares[2]) && (squares[4] == squares [6]))) && (squares[4] != "*"))
+          {console.log(squares[4] + " is the winner!"); //finds diagonal wins
+          callbackFn();}  
+          else if ((squares[i * 3] != "*") && ((squares[i * 3] == squares[i * 3 + 1]) && 
+              (squares[i * 3] == squares[i * 3 + 2]))) //finds horizontal wins
+               {console.log(squares[i*3] + " is the winner!");
+           return [{x:address[i*3][0], y:address[i*3][1]}, {x:address[(i*3)+1][0], y:address[(i*3)+1][1]}, {x:address[(i*3)+2][0], y:address[(i*3)+2][1]}];
+           callbackFn();
+          }
+              else if (((squares[i] != "*")&&(squares[i] == squares[i+3])) && (squares[i] == squares [i+6])) //finds vertical wins
+          {console.log(squares[i] + " is the winner!");
+          return [{x:address[i][0], y:address[i][1]}, {x:address[i+3][0], y:address[i+3][1]}, {x:address[i+6][0], y:address[i+6][1]}];
+          callbackFn();
+          }
         }
-                for (var i = 0; i<9; i++){
-        if (i % 3 == 1)   
-           if(squares[i] == squares[i+3])
-              if (squares[i+3] == squares [i+6])
-                if (squares[i] != "*")
-          {console.log(squares[1] + " is the winner!");}
-        }
-	}
-	this.show = function(){
+      } 
+    this.show = function(){
         var row0 = String(squares[0])+" "+String(squares[1])+" "+String(squares[2]);
         var row1 = String(squares[3])+" "+String(squares[4])+" "+String(squares[5]);
         var row2 = String(squares[6])+" "+String(squares[7])+" "+String(squares[8]);
-		console.log(row2+"\n"+" "+row1+"\n"+" "+row0);
-
-	}
+        console.log(row2+"\n"+" "+row1+"\n"+" "+row0);
+ 
+    }
     this.clear = function(){
         squares = ["*", "*", "*", "*", "*", "*", "*", "*", "*"];
         this.show();
     }
 }
-var game = new TicTacToeBoard();
+
+var gameOverMsg = function(){alert("Game Over!");}
+var game = new TicTacToeBoard(gameOverMsg);
 return game;
 })();
+
 
